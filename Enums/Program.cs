f6224c84fbee63370a2545c772bbef23721c681b
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-06-21 14:17:47 +0200

    Add project files.

---
+++ Enums/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Enums
+{
+    internal class Program
+        
+    {
+        enum Suits
+        {
+            Heart,     //0
+            Diamond,   //1
+            Club,      //2
+            Spade      //3
+        }
+        static void DrawAce(Suits suit)
+        {
+            if (suit == Suits.Heart) 
+            {
+                Console.WriteLine($"╭───────╮");
+                Console.WriteLine($"│A      │");
+                Console.WriteLine($"│♥      │");
+                Console.WriteLine($"│   ♥   │");
+                Console.WriteLine($"│      ♥│");
+                Console.WriteLine($"│      A│");
+                Console.WriteLine($"╰───────╯");
+                return; 
+            }
+            if (suit == Suits.Diamond)
+            {
+                Console.WriteLine($"╭───────╮");
+                Console.WriteLine($"│A      │");
+                Console.WriteLine($"│♦      │");
+                Console.WriteLine($"│   ♦   │");
+                Console.WriteLine($"│      ♦│");
+                Console.WriteLine($"│      A│");
+                Console.WriteLine($"╰───────╯");
+                return;
+            }
+            if (suit == Suits.Club)
+            {
+                Console.WriteLine($"╭───────╮");
+                Console.WriteLine($"│A      │");
+                Console.WriteLine($"│♣      │");
+                Console.WriteLine($"│   ♣   │");
+                Console.WriteLine($"│      ♣│");
+                Console.WriteLine($"│      A│");
+                Console.WriteLine($"╰───────╯");
+                return;
+            }
+            if (suit == Suits.Club)
+                Console.WriteLine($"╭───────╮");
+                Console.WriteLine($"│A      │");
+                Console.WriteLine($"│♠      │");
+                Console.WriteLine($"│   ♠   │");
+                Console.WriteLine($"│      ♠│");
+                Console.WriteLine($"│      A│");
+                Console.WriteLine($"╰───────╯");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+        static void Main(string[] args)
+        {
+            DrawAce(Suits.Heart);
+        }
+    }
+}
